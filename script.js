1decb245aead76b292e933c9af748acf563dbcf4
Kalysbek <rrkalysbe@gmail.com>
2024-02-15 10:18:54 +0600

    first

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const data = [ 
+const data = [
     {
         id: 18560,
         created_At: '2022-01-26T09:21:34.516513',
@@ -8,13 +8,13 @@ const data = [
         side2_Room: 'Зал №1',
         side3_Name: null,
         side3_Room: null,
-        side4_Name: null,   
+        side4_Name: null,
         side4_Room: null,
         date: '2022-02-01T00:00:00',
         start_Time: '13:30',
         end_Time: '14:30',
         case_Details: null,
-        fullname: 'Бегалиев Тилек Ишеналиевич'   
+        fullname: 'Бегалиев Тилек Ишеналиевич'
     },
     {
         id: 18561,
@@ -25,13 +25,13 @@ const data = [
         side2_Room: 'Зал №1',
         side3_Name: null,
         side3_Room: null,
-        side4_Name: null,   
+        side4_Name: null,
         side4_Room: null,
         date: '2022-02-01T00:00:00',
         start_Time: '13:30',
         end_Time: '14:30',
         case_Details: null,
-        fullname: 'Бегалиев Тилек Ишеналиевич'   
+        fullname: 'Бегалиев Тилек Ишеналиевич'
     },
     {
         id: 18562,
@@ -42,13 +42,13 @@ const data = [
         side2_Room: 'Зал №1',
         side3_Name: null,
         side3_Room: null,
-        side4_Name: null,   
+        side4_Name: null,
         side4_Room: null,
         date: '2022-02-01T00:00:00',
         start_Time: '13:30',
         end_Time: '14:30',
         case_Details: null,
-        fullname: 'Бегалиев Тилек Ишеналиевич'   
+        fullname: 'Бегалиев Тилек Ишеналиевич'
     },
     {
         id: 18563,
@@ -59,13 +59,13 @@ const data = [
         side2_Room: 'Зал №1',
         side3_Name: null,
         side3_Room: null,
-        side4_Name: null,   
+        side4_Name: null,
         side4_Room: null,
         date: '2022-02-01T00:00:00',
         start_Time: '13:30',
         end_Time: '14:30',
         case_Details: null,
-        fullname: 'Бегалиев Тилек Ишеналиевич'   
+        fullname: 'Бегалиев Тилек Ишеналиевич'
     },
     {
         id: 18564,
@@ -76,13 +76,13 @@ const data = [
         side2_Room: 'Зал №1',
         side3_Name: null,
         side3_Room: null,
-        side4_Name: null,   
+        side4_Name: null,
         side4_Room: null,
         date: '2022-02-01T00:00:00',
         start_Time: '13:30',
         end_Time: '14:30',
         case_Details: null,
-        fullname: 'Бегалиев Тилек Ишеналиевич'   
+        fullname: 'Бегалиев Тилек Ишеналиевич'
     }
     ,
     {
@@ -94,14 +94,14 @@ const data = [
         side2_Room: 'Зал №1',
         side3_Name: null,
         side3_Room: null,
-        side4_Name: null,   
+        side4_Name: null,
         side4_Room: null,
         date: '2022-02-01T00:00:00',
         start_Time: '13:30',
         end_Time: '14:30',
         case_Details: null,
-        fullname: 'Бегалиев Тилек Ишеналиевич'   
-    } , 
+        fullname: 'Бегалиев Тилек Ишеналиевич'
+    },
     {
         id: 18566,
         created_At: '2022-01-26T09:21:34.516513',
@@ -111,136 +111,135 @@ const data = [
         side2_Room: 'Зал №1',
         side3_Name: null,
         side3_Room: null,
-        side4_Name: null,   
+        side4_Name: null,
         side4_Room: null,
         date: '2022-02-01T00:00:00',
         start_Time: '13:30',
         end_Time: '14:30',
         case_Details: null,
-        fullname: 'Бегалиев Тилек Ишеналиевич'   
-    }, 
-    ]
-   
-    const categories = Array.from(new Set(data.map(item => item.side1_Name)));
-        const buttonContainer = document.getElementById('button-container');
-        categories.forEach(category => {
-        const count = data.filter(item => item.side1_Name === category).length;
-        const Card = document.createElement('button');
-        Card.style.width = 18 + 'rem';
-        Card.classList.add('category-button','btn','border','ms-1','mb-1')
-        Card.innerHTML = `
-            <div>
-                <h5 class="card-title text-center">${category}</h5>
-            </div>
-            <span class="fs-2 ">
-                ${count}
-            </span>
-        `
-      
-        Card.addEventListener('click', () => {
-                const filteredItems = data.filter(item => item.side1_Name === category);
-                updateList(filteredItems)
-        });
-        buttonContainer.appendChild(Card);
-        });
+        fullname: 'Бегалиев Тилек Ишеналиевич'
+    },
+]
 
-        const allButton = document.createElement('button');
-        allButton.style.width = 18 + 'rem'
-        allButton.classList.add('category-button','btn','border','active','ms-1','mb-1')
-        allButton.innerHTML = `
-            <div>
-                <h5 class="card-title text-center">Все заявки</h5>
-            </div>
-            <span class="fs-2">
-                ${data.length}
-            </span>
-        `
-        allButton.addEventListener('click', () => {
-                updateList(data)
-        });
-        buttonContainer.appendChild(allButton);
-
-        function updateList(newData) {
-            const tableBody = document.getElementById('accordion-data')
-            tableBody.innerHTML = ''
-                for (let item  of newData) {
-                const row = document.createElement('div');
-                row.style.width = 100 + '%'
-                row.classList.add('accordion-item')
-                row.innerHTML = `
-                <h2 class="accordion-header" id="heading${item.id}">
-                <button class="accordion-button collapsed shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${item.id}" aria-expanded="false" aria-controls="collapse${item.id}">
-                    <div class='d-flex flex-column'>
-                        <span class=''><strong>Номер приказа:</strong> ${item.id}</span>
-                        <span class='me-5'><strong>Дата:</strong> ${item.date.slice(0,10)}</span>
-                        <span><strong>Место проведения суда: </strong> ${item.side1_Name} - ${item.side1_Room}</span>
-                    </div>
-
-                </button>
-                </h2>
-                    <div id="collapse${item.id}" class="accordion-collapse collapse" aria-labelledby="heading${item.id}" data-bs-parent="#accordionExample">
-                    <div class="accordion-body">
-                        <div>
-                            <span><b>Дата создания приказа:</b>${item.created_At.slice(0,10)}</span>
-                            <div>
-                                <div>
-                                    ${item.side2_Name ? item.side2_Name + ' - ' : ''}  ${item.side2_Room ? item.side2_Room : ''}
-                                </div>
-                                <div>
-                                    ${item.side3_Name ? item.side3_Name + ' - ' : ''}  ${item.side3_Room ? item.side3_Room : ''}
-                                </div>
-                                <div>
-                                    ${item.side4_Name ? item.side4_Name + ' - ' : ''}  ${item.side4_Room ? item.side4_Room : ''}
-                                </div>
-                            </div>
-                        <div>
-                            <span><b>Дата:</b> ${item.date.slice(0,10)}</span>
-                            <span><b>Начало:</b> ${item.start_Time}</span>   
-                            <span><b>Конец:</b> ${item.end_Time}</span>       
-                        </div>
-                        <span>
-                        ${item.case_Details ? item.case_Details : ''}    
-                        <span>
-                        <div>
-                        <span><b>Ф И О:</b> ${item.fullname}</span>    
-                        </div>    
-                        </div>
-                    </div>
-                    </div>
-      
-                `;
-                tableBody.appendChild(row);
-                }
-                
-        }
-
-        let allButtons = document.querySelectorAll('.category-button');
-        allButtons.forEach((item) => {
-            item.addEventListener('click', (e)=> {
-                allButtons.forEach((btn) => {
-                    btn.classList.remove('active')
-                })
-                item.classList.add('active')
-            })
-        })
-
-        updateList(data)
-
-        const key = "68909dc31ae0b122e1e951c4431a2dfe"
-        let message = ''
-        function pushMessageTelegram() {
-            if(localStorage.getItem('lengthData') < data.length) {
-                localStorage.setItem('lengthData',data.length)
-                  message = `Поступила новая заявка в ${data.at(-1).side1_Name} с номером: ${data.at(-1).id}`
-                  let xhttp = new XMLHttpRequest();
-                  xhttp.open("GET",`http://pushmebot.ru/send?key=${key}&message=${message}`,true)
-                  xhttp.send()
-                updateList(data)
-            }
-            else if (localStorage.getItem('lengthData') > data.length) {
-                localStorage.setItem('lengthData',data.length)
-                updateList(data)
-            }
-        }
-         setInterval(pushMessageTelegram,5000)
+const categories = Array.from(new Set(data.map(item => item.side1_Name)));
+    const SelectOrder = document.getElementById('select-order');
+    categories.forEach(category => {
+    const count = data.filter(item => item.side1_Name === category).length;
+    const Card = document.createElement('option');
+    Card.value = category
+    Card.innerHTML = category 
+    
+         
  
+       
+    
+
+    SelectOrder.addEventListener('change', (e) => {
+            if(e.target.value === 'Все заявки') {
+                updateList(data)
+            } else {
+                const filteredItems = data.filter(item => item.side1_Name === e.target.value);
+                updateList(filteredItems)
+            }
+     
+    });
+    SelectOrder.appendChild(Card);
+    });
+
+
+
+
+
+
+
+
+function updateList(newData) {
+    const tableBody = document.getElementById('table')
+    let predTr = document.querySelectorAll('.parse')
+     predTr.forEach(item => {
+        item.parentNode.removeChild(item)
+     })
+    for (let item of newData) {
+       
+        const row = document.createElement('tr');
+        row.style.width = 100 + '%'
+        row.classList.add('table-light','parse')
+        row.innerHTML = `
+                <td>${item.id}</td>
+                <td>${item.date.slice(0, 10)}</td>
+                <td>${item.side1_Name} - ${item.side1_Room}</td>
+                <td align="right">
+                <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="ViewDetails(${item.id})">
+                   Открыть
+                </button>
+                </td>
+                `;
+        tableBody.appendChild(row);
+    }
+
+
+}
+function ViewDetails(id) {
+    const ModalBody = document.getElementById('modal-body')
+    ModalBody.innerHTML = ''
+    data.forEach(item => {
+
+        if (item.id == id) {
+           
+            let body = `
+         
+            <div><b>Номер приказа: </b> ${item.id}</div>
+            <div><b>Дата создания приказа: </b>${item.created_At.slice(0, 10)}</div>
+       
+                <div>
+                    <b>Место проведения: </b>
+                    ${item.side2_Name ? item.side2_Name + ' - ' : ''}  ${item.side2_Room ? item.side2_Room : ''}
+                </div>
+                <div>
+                    ${item.side3_Name ? item.side3_Name + ' - ' : ''}  ${item.side3_Room ? item.side3_Room : ''}
+                </div>
+                <div>
+                    ${item.side4_Name ? item.side4_Name + ' - ' : ''}  ${item.side4_Room ? item.side4_Room : ''}
+                </div>
+         
+
+            <div><b>Дата:</b> ${item.date.slice(0, 10)}</div>
+            <div><b>Начало:</b> ${item.start_Time}</span>   
+            <div><b>Конец:</b> ${item.end_Time}</div>       
+        </div>
+        <span>
+        ${item.case_Details ? item.case_Details : ''}    
+        <span>
+        <div>
+        <span><b>Ф И О:</b> ${item.fullname}</span>    
+        </div>    
+        </div>
+            `
+            ModalBody.insertAdjacentHTML('afterbegin', body)
+        }
+    })
+
+
+ 
+}
+
+
+updateList(data)
+
+const key = "68909dc31ae0b122e1e951c4431a2dfe"
+let message = ''
+function pushMessageTelegram() {
+    if (localStorage.getItem('lengthData') < data.length) {
+        localStorage.setItem('lengthData', data.length)
+        message = `Поступила новая заявка в ${data.at(-1).side1_Name} с номером: ${data.at(-1).id}`
+        let xhttp = new XMLHttpRequest();
+        xhttp.open("GET", `http://pushmebot.ru/send?key=${key}&message=${message}`, true)
+        xhttp.send()
+        updateList(data)
+    }
+    else if (localStorage.getItem('lengthData') > data.length) {
+        localStorage.setItem('lengthData', data.length)
+        updateList(data)
+    }
+}
+setInterval(pushMessageTelegram, 5000)
